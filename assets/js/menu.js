f5b842769f292192b8635ea5a06fcbf95ca4bee0
mati <jmsalinas@uc.cl>
2019-05-08 21:36:40 -0400

    fonts

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,40 +1,25 @@
+// MENU
 
+	var menuShow = $('.menu__responsive').children(".menu__responsive--hidden").hasClass('show');
 
-var checker = true;
-var boton1 = document.getElementById("menuicon");
-var boton2 = document.getElementById("hamburger");
+	function desplazar (){
+		$('.menu__responsive--hidden').toggleClass('show');
+		$('#hamburger').toggleClass('hamburguer-slide');
+	}
 
+	$('#hamburger').on('click', function(e){
+		desplazar();
+		e.stopPropagation();
+		menuShow = $('.menu__responsive').children(".menu__responsive--hidden").hasClass('show');
+		console.log(menuShow)
+	})
 
-
-function desplazar(){
-    targetLeft = checker ? "158px" : "20px";
-    targetRight = checker ? "130px" : "20px";
-    targetMiddle = checker ? "148px" : "20px";
-    $("#hamburger").animate({left: targetLeft},400);
-    $("#menuicon2").animate({left: targetRight},400);
-    $("#menuicon3").fadeToggle(400);
-    $("#menuicon3").animate({left: targetMiddle},{queue: false},400);
-    $('.menu-hide').toggleClass('show');
-    checker ? (checker = false) : (checker = true);
-}
-
-
-
-  $(".nav-menu-res").click(function () {
-  $(document).ready(function(){
-  desplazar();
-});
-});
-
-
-
-    $(document).ready(function(){
-    window.addEventListener('click', function(e){
-      var menu= document.getElementsByClassName("menu-hide show");
-   if (e.target.className != "menu-hide show" && checker===false && e.target!= boton1 && e.target != boton2) {
-        desplazar();
-
-     }
-
-    })
-})
+	window.addEventListener('click', function(e){
+		// var menuHide = $('.menu-hide');
+		if (menuShow === true && e.target.className != 'menu__responsive--hidden show') {
+			// desplazar();
+			$('.menu__responsive--hidden').removeClass('show');
+			$('#hamburger').removeClass('hamburguer-slide');
+		}
+		console.log(e.target)
+	})
