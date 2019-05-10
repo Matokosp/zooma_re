e3fee2456018d8cf2a3d7afa9a42a4de398fae79
mati <jmsalinas@uc.cl>
2019-05-10 17:58:20 -0400

    changing text

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,11 +1,79 @@
-// GIF RESET
+// // GIF RESET
+//
+// var img = document.createElement("img"),
+//     imageUrl = "#";
+// img.src = imageUrl;
+// document.body.appendChild(img);
+//
+// window.restartAnim = function () {
+//     img.src = "";
+//     img.src = imageUrl;
+// }
 
-var img = document.createElement("img"),
-    imageUrl = "http://i73.photobucket.com/albums/i231/charma13/love240.gif";
-img.src = imageUrl;
-document.body.appendChild(img);
 
-window.restartAnim = function () {
-    img.src = "";
-    img.src = imageUrl;
-}
+// TEXT CHANGE
+
+(function() {
+
+    var quoteOne = $(".quote_one");
+    var quoteIndex = -1;
+
+    function showNextQuote() {
+        ++quoteIndex;
+        quoteOne.eq(quoteIndex % quoteOne.length)
+            .fadeIn(1500)
+            .delay(1500)
+            .fadeOut(1500, showNextQuote);
+    }
+
+    showNextQuote();
+
+})();
+
+(function() {
+
+    var quoteTwo = $(".quote_two");
+    var quoteIndex = -1;
+
+    function showNextQuote() {
+        ++quoteIndex;
+        quoteTwo.eq(quoteIndex % quoteTwo.length)
+            .fadeIn(1500)
+            .delay(1500)
+            .fadeOut(1500, showNextQuote);
+    }
+
+    showNextQuote();
+
+})();
+
+// var messages = [
+//     "Hello!",
+//     "This is a website!",
+//     "You are now going to be redirected.",
+// ].reverse();
+
+// textSequence(0);
+// function textSequence(i) {
+//
+//   if (messages.length > i) {
+//     setTimeout(function() {
+//       document.getElementById("message").innerHTML = messages[i];
+//       textSequence(++i);
+//     }, 2000);
+//   } else if (messages.length == i) {
+//     textSequence(0);
+//   }
+//
+//   }
+
+// function nextMsg() {
+//     // if (messages.length != 0) {
+//       $('#message').html(messages.pop()).fadeIn(500).delay(1000).fadeOut(500, nextMsg);
+//     // } else {
+//     //
+//     // }
+// };
+//
+// $('#message').hide();
+// nextMsg();
